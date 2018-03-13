a04110fd5944032b3a18fd5be30b823d38ee7fed
Sammers21 <titantins@gmail.com>
2018-03-13 23:07:17 +0300

    implement /quotes + proper verticle start

--- src/main/java/io/vertx/gsoc2018/qotd/QuoteOfTheDayVerticle.java
+++ src/main/java/io/vertx/gsoc2018/qotd/QuoteOfTheDayVerticle.java
@@ -2,11 +2,15 @@ package io.vertx.gsoc2018.qotd;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.sql.SQLConnection;
+import io.vertx.ext.web.Router;
 
 /**
  * @author Thomas Segismont
@@ -25,6 +29,38 @@ public class QuoteOfTheDayVerticle extends AbstractVerticle {
 
     jdbcClient = JDBCClient.createShared(vertx, jdbcConfig);
 
+    // web server setup
+    HttpServer server = vertx.createHttpServer();
+    Router router = Router.router(vertx);
+
+    router.route("/quotes").handler(routingContext -> {
+      HttpServerResponse response = routingContext.response();
+      jdbcClient.getConnection(getConn -> {
+        if (getConn.succeeded()) {
+          SQLConnection connection = getConn.result();
+          String sqlToExecute = "SELECT * FROM quotes";
+          connection.query(sqlToExecute, query -> {
+            if (query.succeeded()) {
+              JsonArray responseBody = new JsonArray();
+              query.result().getRows().forEach(responseBody::add);
+              logger.info("fine");
+              response.end(responseBody.toBuffer());
+            } else {
+              logger.error("failed in query executing", query.cause());
+              responseWithError(response);
+            }
+            connection.close();
+          });
+        } else {
+          logger.error("failed in db connection establishing", getConn.cause());
+          responseWithError(response);
+        }
+      });
+    });
+
+    int port = config().getInteger("http.port", 8080);
+    server.requestHandler(router::accept).listen(port);
+
     Future<Void> initSchema = runScript("db.sql");
 
     initSchema.setHandler(initSchemaResult -> {
@@ -34,6 +70,7 @@ public class QuoteOfTheDayVerticle extends AbstractVerticle {
         importData.setHandler(dataImportResult -> {
           if (dataImportResult.succeeded()) {
             logger.info("initial data had been loaded");
+            startFuture.complete();
           } else {
             logger.error("failed in attempt to load initial data", dataImportResult.cause());
           }
@@ -42,11 +79,11 @@ public class QuoteOfTheDayVerticle extends AbstractVerticle {
         logger.error("failed in attempt to load initial schema", initSchemaResult.cause());
       }
     });
+  }
 
-    int port = config().getInteger("http.port", 8080);
-
-    // TODO setup database and web server and eventually...
-    startFuture.complete();
+  private void responseWithError(HttpServerResponse response) {
+    response.setStatusCode(400);
+    response.end("Something went wrong");
   }
 
   private Future<Void> runScript(String script) {
