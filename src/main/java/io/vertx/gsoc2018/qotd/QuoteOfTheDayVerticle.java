d2b6ce5172e18966d7b853d9ec34cd10172f9727
Sammers21 <titantins@gmail.com>
2018-03-16 15:08:25 +0300

    setup db and http server sequentially

--- src/main/java/io/vertx/gsoc2018/qotd/QuoteOfTheDayVerticle.java
+++ src/main/java/io/vertx/gsoc2018/qotd/QuoteOfTheDayVerticle.java
@@ -1,7 +1,6 @@
 package io.vertx.gsoc2018.qotd;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.core.eventbus.MessageConsumer;
 import io.vertx.core.http.HttpServer;
@@ -13,6 +12,7 @@ import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.sql.SQLConnection;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 /**
  * @author Thomas Segismont
@@ -30,16 +30,6 @@ public class QuoteOfTheDayVerticle extends AbstractVerticle {
 
   @Override
   public void start(Future<Void> startFuture) {
-    Future<Void> dbReady = Future.future();
-    Future<Void> httpServerReady = Future.future();
-
-    CompositeFuture.all(dbReady, httpServerReady).setHandler(result -> {
-      if (result.succeeded()) {
-        startFuture.complete();
-      } else {
-        startFuture.fail(result.cause());
-      }
-    });
 
     JsonObject jdbcConfig = new JsonObject()
         .put("url", "jdbc:h2:mem:test;DATABASE_TO_UPPER=false;DB_CLOSE_DELAY=-1")
@@ -47,81 +37,36 @@ public class QuoteOfTheDayVerticle extends AbstractVerticle {
 
     jdbcClient = JDBCClient.createShared(vertx, jdbcConfig);
 
+    Future<Void> initSchema = runScript("db.sql");
+
+    initSchema
+      .compose(result -> {
+        logger.info("initial schema successfully had been loaded");
+        return runScript("import.sql");
+      })
+      .compose(result -> {
+        logger.info("initial data had been loaded");
+        return startHttpServer();
+      })
+      .compose(result -> {
+        logger.info("http server started");
+        startFuture.complete();
+      }, startFuture);
+  }
+
+  private void responseWithError(HttpServerResponse response) {
+    response.setStatusCode(400);
+    response.end("Something went wrong");
+  }
+
+  private Future<HttpServer> startHttpServer() {
+    Future<HttpServer> serverStarted = Future.future();
     // web server setup
     HttpServer server = vertx.createHttpServer();
     Router router = Router.router(vertx);
 
-    router.post(QUOTES_PATH).handler(routingContext -> {
-      logger.info("receive post");
-      HttpServerResponse response = routingContext.response();
-      routingContext.request().bodyHandler(requestBody -> {
-        JsonObject request = requestBody.toJsonObject();
-        String text = request.getString(TEXT_FILED);
-        if (text == null) {
-          response.setStatusCode(404);
-          response.end("json in a POST request should have a 'text' filed");
-          return;
-        }
-
-        String author = request.getString(AUTHOR_FILED);
-        if (author == null) {
-          author = DEFAULT_AUTHOR_VALUE;
-        }
-
-        String finalAuthor = author;
-        jdbcClient.getConnection(getConn -> {
-          if (getConn.succeeded()) {
-            SQLConnection connection = getConn.result();
-            String sqlToExecute = "INSERT INTO quotes (text,author) VALUES (?, ?);";
-            JsonArray sqlParams = new JsonArray().add(text).add(finalAuthor);
-            connection.updateWithParams(sqlToExecute, sqlParams, exec -> {
-              if (exec.succeeded()) {
-                response.setStatusCode(200);
-                response.end();
-
-                // notify subscribers about database update
-                JsonObject newQuote = new JsonObject()
-                  .put(AUTHOR_FILED, finalAuthor)
-                  .put(TEXT_FILED, text);
-                vertx.eventBus().publish(DB_UPDATES_ADDRESS, newQuote);
-              } else {
-                logger.error("failed in query executing", exec.cause());
-                responseWithError(response);
-              }
-              connection.close();
-            });
-          } else {
-            logger.error("failed in db connection establishing", getConn.cause());
-            responseWithError(response);
-          }
-        });
-
-      });
-    });
-
-    router.get(QUOTES_PATH).handler(routingContext -> {
-      HttpServerResponse response = routingContext.response();
-      jdbcClient.getConnection(getConn -> {
-        if (getConn.succeeded()) {
-          SQLConnection connection = getConn.result();
-          String sqlToExecute = "SELECT * FROM quotes";
-          connection.query(sqlToExecute, query -> {
-            if (query.succeeded()) {
-              JsonArray responseBody = new JsonArray();
-              query.result().getRows().forEach(responseBody::add);
-              response.end(responseBody.toBuffer());
-            } else {
-              logger.error("failed in query executing", query.cause());
-              responseWithError(response);
-            }
-            connection.close();
-          });
-        } else {
-          logger.error("failed in db connection establishing", getConn.cause());
-          responseWithError(response);
-        }
-      });
-    });
+    router.post(QUOTES_PATH).handler(this::handlePostQuotes);
+    router.get(QUOTES_PATH).handler(this::handleGetQuotes);
 
     server.websocketHandler(ws -> {
       if (ws.path().equals("/realtime")) {
@@ -137,41 +82,8 @@ public class QuoteOfTheDayVerticle extends AbstractVerticle {
 
 
     int port = config().getInteger("http.port", 8080);
-    server.requestHandler(router::accept).listen(port, listenHandler -> {
-      if (listenHandler.succeeded()) {
-        httpServerReady.complete();
-        logger.info("HTTP successfully started on port " + port);
-      } else {
-        httpServerReady.fail(listenHandler.cause());
-        logger.error("Failed to start server on port " + port, listenHandler.cause());
-      }
-    });
-
-    Future<Void> initSchema = runScript("db.sql");
-
-    initSchema.setHandler(initSchemaResult -> {
-      if (initSchemaResult.succeeded()) {
-        logger.info("initial schema successfully had been loaded");
-        Future<Void> importData = runScript("import.sql");
-        importData.setHandler(dataImportResult -> {
-          if (dataImportResult.succeeded()) {
-            dbReady.complete();
-            logger.info("initial data had been loaded");
-          } else {
-            dbReady.fail(dataImportResult.cause());
-            logger.error("failed in attempt to load initial data", dataImportResult.cause());
-          }
-        });
-      } else {
-        dbReady.fail(initSchema.cause());
-        logger.error("failed in attempt to load initial schema", initSchemaResult.cause());
-      }
-    });
-  }
-
-  private void responseWithError(HttpServerResponse response) {
-    response.setStatusCode(400);
-    response.end("Something went wrong");
+    server.requestHandler(router::accept).listen(port, serverStarted.completer());
+    return serverStarted;
   }
 
   private Future<Void> runScript(String script) {
@@ -206,5 +118,77 @@ public class QuoteOfTheDayVerticle extends AbstractVerticle {
   @Override
   public void stop() {
     jdbcClient.close();
+  }
+
+  private void handlePostQuotes(RoutingContext routingContext) {
+    logger.info("receive post");
+    HttpServerResponse response = routingContext.response();
+    routingContext.request().bodyHandler(requestBody -> {
+      JsonObject request = requestBody.toJsonObject();
+      String text = request.getString(TEXT_FILED);
+      if (text == null) {
+        response.setStatusCode(404);
+        response.end("json in a POST request should have a 'text' filed");
+        return;
+      }
+
+      String author = request.getString(AUTHOR_FILED);
+      if (author == null) {
+        author = DEFAULT_AUTHOR_VALUE;
+      }
+
+      String finalAuthor = author;
+      jdbcClient.getConnection(getConn -> {
+        if (getConn.succeeded()) {
+          SQLConnection connection = getConn.result();
+          String sqlToExecute = "INSERT INTO quotes (text,author) VALUES (?, ?);";
+          JsonArray sqlParams = new JsonArray().add(text).add(finalAuthor);
+          connection.updateWithParams(sqlToExecute, sqlParams, exec -> {
+            if (exec.succeeded()) {
+              response.setStatusCode(200);
+              response.end();
+
+              // notify subscribers about database update
+              JsonObject newQuote = new JsonObject()
+                .put(AUTHOR_FILED, finalAuthor)
+                .put(TEXT_FILED, text);
+              vertx.eventBus().publish(DB_UPDATES_ADDRESS, newQuote);
+            } else {
+              logger.error("failed in query executing", exec.cause());
+              responseWithError(response);
+            }
+            connection.close();
+          });
+        } else {
+          logger.error("failed in db connection establishing", getConn.cause());
+          responseWithError(response);
+        }
+      });
+
+    });
+  }
+
+  private void handleGetQuotes(RoutingContext routingContext) {
+    HttpServerResponse response = routingContext.response();
+    jdbcClient.getConnection(getConn -> {
+      if (getConn.succeeded()) {
+        SQLConnection connection = getConn.result();
+        String sqlToExecute = "SELECT * FROM quotes";
+        connection.query(sqlToExecute, query -> {
+          if (query.succeeded()) {
+            JsonArray responseBody = new JsonArray();
+            query.result().getRows().forEach(responseBody::add);
+            response.end(responseBody.toBuffer());
+          } else {
+            logger.error("failed in query executing", query.cause());
+            responseWithError(response);
+          }
+          connection.close();
+        });
+      } else {
+        logger.error("failed in db connection establishing", getConn.cause());
+        responseWithError(response);
+      }
+    });
   }
 }
